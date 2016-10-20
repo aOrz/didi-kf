889d4cc534a1cbba8d7f7c619436dac0fff62a82
4013465w <4013465w@sina.com>
2016-10-20 13:43:53 +0800

    add glob

--- bin/kf.js
+++ bin/kf.js
@@ -3,11 +3,12 @@ var curPath = process.cwd();
 var colors = require('colors');
 var xtpl = require('xtpl');
 var yargs = require('yargs');
+var glob = require('glob');
 var kf = require('../src/index.js');
 
-// glob('*',{nodir: false},function (error,files) {
-//   console.log(files)
-// })
+glob('*',{nodir: false},function (error,files) {
+  console.log(files)
+})
 xtpl.renderFile(path.resolve(curPath,'x.tpl'),{
     title:1
 },function(error,content){
