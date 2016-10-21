d3795d5bec3f5eb5786524daabcd481f14df820e
Rememberautumn <4013465w@sina.com>
2016-10-21 08:06:23 +0800

    允许设置标题

--- bin/kf.js
+++ bin/kf.js
@@ -7,12 +7,16 @@ var log = require('../src/log.js');
 
 var yas = require('yargs')
     .command('init', '文件创建中....', function (yargs) {
-       var argv = yargs.reset().argv;
+       var argv = yargs
+       .reset()
+       .default({t: '客服中心'})
+       .alias('t', 'title')
+       .argv;
       if (!argv._[1]) {
         log.error('请输入文件名哦！');
       }
       kf.init(argv._[1], null, false, {
-        title:'sssss'
+        title:argv.title
       });
       kf.checkFiles();
       kf.copyFile()
